cffa15dbb3402bdefdfc3b20bef222981ff97a1b
azradurmus <azra.durmus@yahoo.com>
2021-04-13 08:55:00 +0200

    Add Project

---
+++ Assets/MyGame/Scripts/ColorChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChange : MonoBehaviour
+{
+    public Image color;
+
+    public void ChangeColor()
+    {
+        int ranNumber;
+
+        var colors = new List<Color>()
+        {
+            new Color32(255, 205, 25, 100),
+            new Color32(95, 34, 0, 100),
+            new Color32(207, 63, 21, 100),
+            new Color32(102, 47, 84, 100)
+        };
+
+        if (Input.GetKeyDown("space"))
+        {
+            ranNumber = Random.Range(0, colors.Count);
+            color.color = colors[ranNumber];
+        }
+
+    }
+
+    public void Update()
+    {
+        ChangeColor();
+    }
+}
---
+++ Assets/MyGame/Scripts/Controller.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public InputField A;
+    public InputField B;
+    public float result;
+    public Text t;
+
+    public Text error;
+    public float nr1;
+    public float nr2;
+    public Color grey = Color.grey;
+    public Color red = Color.red;
+
+    public void ChangeScenetoColor()
+    {
+        SceneManager.LoadScene("01Colorscene");
+
+    }
+
+    public void ChangeScenetoNumber()
+    {
+        SceneManager.LoadScene("02Numberscene");
+
+    }
+
+    public void ChangeScenetoWelcome()
+    {
+        SceneManager.LoadScene("00Welcomescene");
+    }
+
+    public void Add()
+    {
+
+        try
+		{
+            nr1 = float.Parse(A.text);
+            A.image.color = grey;
+        }
+
+        catch (System.Exception)
+		{
+            A.image.color = red;
+            error.text = "Error! Enter Number";
+		}
+
+        try
+		{
+            nr2 = float.Parse(B.text);
+            B.image.color = grey;
+        }
+
+        catch (System.Exception)
+		{
+            B.image.color = red;
+            error.text = "Error! Enter Number";
+		}
+
+
+        result = float.Parse(A.text) + float.Parse(B.text);
+        t.text = result.ToString();
+        
+    }
+
+}
