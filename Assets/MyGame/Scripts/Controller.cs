efb41c85b0319b398caf8c4e88e6de5164a13132
azradurmus <58288502+azradurmus@users.noreply.github.com>
2021-04-13 09:46:55 +0200

    Update Controller.cs

--- Assets/MyGame/Scripts/Controller.cs
+++ Assets/MyGame/Scripts/Controller.cs
@@ -1,70 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Controller : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
-    public InputField A;
-    public InputField B;
-    public float result;
-    public Text t;
 
-    public Text error;
-    public float nr1;
-    public float nr2;
-    public Color grey = Color.grey;
-    public Color red = Color.red;
-
-    public void ChangeScenetoColor()
+    public void ChangeScenetoFrage()
     {
-        SceneManager.LoadScene("01Colorscene");
+        SceneManager.LoadScene("01Frage2");
 
     }
 
-    public void ChangeScenetoNumber()
+    public void ChangeScenetoFinal()
     {
-        SceneManager.LoadScene("02Numberscene");
+        SceneManager.LoadScene("02FinalScreen");
 
     }
 
-    public void ChangeScenetoWelcome()
-    {
-        SceneManager.LoadScene("00Welcomescene");
-    }
-
-    public void Add()
-    {
-
-        try
-		{
-            nr1 = float.Parse(A.text);
-            A.image.color = grey;
-        }
-
-        catch (System.Exception)
-		{
-            A.image.color = red;
-            error.text = "Error! Enter Number";
-		}
-
-        try
-		{
-            nr2 = float.Parse(B.text);
-            B.image.color = grey;
-        }
-
-        catch (System.Exception)
-		{
-            B.image.color = red;
-            error.text = "Error! Enter Number";
-		}
-
-
-        result = float.Parse(A.text) + float.Parse(B.text);
-        t.text = result.ToString();
-        
-    }
 
 }
